c6efb9c3775b4b88b87b4f6b0bd52a3e95989245
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-12-15 14:37:03 +0900

    左右の動きを設定

---
+++ script.js
@@ -0,0 +1,36 @@
+let character = document.getElementById("character");
+let game = document.getElementById("game");
+let interval;
+let both = 0;
+let counter = 0;
+let currentBlocks = [];
+
+function moveLeft(){
+  let left = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
+  if (left > 0){
+       character.style.left = left - 2 + "px";
+  }
+}
+function moveRight(){
+    let left = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
+    if(left < 380){
+        character.style.left = left + 2 + 'px';
+    }
+}
+
+document.addEventListener('keydown', event => {
+    if(both==0){
+        both++;
+        if(event.key === "ArrowLeft"){
+            interval = setInterval(moveleft, 1);
+        }
+        if(event.key === "ArrowRight") {
+            interval = setInterval(moveright, 1);
+        }
+    }
+});
+
+document.addEventListener(('keyup', event => {
+   clearInterval(interval);
+   both = 0
+});
